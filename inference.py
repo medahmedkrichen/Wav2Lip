dce5355361f39e85de6b59caae762a0a3b2f2a06
mohamed ahmed Krichen <57727551+medahmedkrichen@users.noreply.github.com>
2024-11-30 03:22:12 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -97,11 +97,6 @@ def face_detect(images):
 					y2 = rect[3]
 					x1 = rect[0]
 					x2 = rect[2]
-					print("###### y1: ",y1)
-					print("###### y2: ",y2)
-					print("###### x1: ",x1)
-					print("###### x2: ",x2)
-					
 				
 					face = np.array(images[i:i + batch_size])[y1:y2, x1:x2]
 					supposed_speaker = frame_per_speaker[i]

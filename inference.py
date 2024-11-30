7bde17c0eedbd3e745a442b2fb79dffc53294780
mohamed ahmed Krichen <57727551+medahmedkrichen@users.noreply.github.com>
2024-11-30 06:43:57 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -108,8 +108,8 @@ def face_detect(images):
 					speaking_speaker = "SPEAKER_00"
 					best_socre = 0
 					for speaker in os.listdir("speaker_images"):
-						if SSIM_similarity(f"speaker_images/{speaker}/average_image.jpg", face)  > best_socre:
-							best_socre = SSIM_similarity(f"speaker_images/{speaker}/average_image.jpg", face) 
+						if SSIM_similarity(f"speaker_images/{speaker}/max_image.jpg", face)  > best_socre:
+							best_socre = SSIM_similarity(f"speaker_images/{speaker}/max_image.jpg", face) 
 							speaking_speaker = speaker
 					if speaking_speaker==supposed_speaker:
 						predictions.extend([rect])
